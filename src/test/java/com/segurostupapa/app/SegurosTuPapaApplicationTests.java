1d52c59bb338b9c57dbd98d53edb1229dca706e0
Juan <juanbohorquez606@outlook.com>
2023-10-09 15:20:03 -0500

    Project Setup

---
+++ src/test/java/com/segurostupapa/app/SegurosTuPapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segurostupapa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurosTuPapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
